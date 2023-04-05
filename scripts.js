f1ff38234af1fa1cecb40e4a0f63e0dfdf4619a2
QennNg <qennng@gmail.com>
2023-04-05 16:01:08 +0800

    Test Brand 3D

--- scripts.js
+++ scripts.js
@@ -6,39 +6,39 @@ const showInfo = () => {
     const locationButton = document.querySelector("#location-button");
     //const text = document.querySelector("#text");
 
-    profileButton.setAttribute("visible", true);
-    setTimeout(() => {
-      webButton.setAttribute("visible", true);
-    }, 150);
-    setTimeout(() => {
-      emailButton.setAttribute("visible", true);
-    }, 300);
-    setTimeout(() => {
-      locationButton.setAttribute("visible", true);
-    }, 450);
+    // profileButton.setAttribute("visible", true);
+    // setTimeout(() => {
+    //   webButton.setAttribute("visible", true);
+    // }, 150);
+    // setTimeout(() => {
+    //   emailButton.setAttribute("visible", true);
+    // }, 300);
+    // setTimeout(() => {
+    //   locationButton.setAttribute("visible", true);
+    // }, 450);
 
-    let currentTab = '';
-    profileButton.addEventListener('click', function (evt) {
-        window.location.href = "https://www.instagram.com/nvqenn/?hl=en";
-        //text.setAttribute("value", "AR, VR solutions and consultation");
-        currentTab = 'profile';
-    });
-    webButton.addEventListener('click', function (evt) {
-        window.location.href = "https://nvqenn.com/";
-        //text.setAttribute("value", "https://softmind.tech");
-        currentTab = 'web';
-    });
-    emailButton.addEventListener('click', function (evt) {
-        window.location.href = "https://www.google.com/gmail/";
-        //text.setAttribute("value", "hello@softmind.tech");
-        currentTab = 'email';
-    });
-    locationButton.addEventListener('click', function (evt) {
-        window.location.href = "https://goo.gl/maps/PsyXqNNvgrY8YyF47";
-        console.log("loc");
-        //text.setAttribute("value", "Vancouver, Canada | Hong Kong");
-        currentTab = 'location';
-    });
+    // let currentTab = '';
+    // profileButton.addEventListener('click', function (evt) {
+    //     window.location.href = "https://www.instagram.com/nvqenn/?hl=en";
+    //     //text.setAttribute("value", "AR, VR solutions and consultation");
+    //     currentTab = 'profile';
+    // });
+    // webButton.addEventListener('click', function (evt) {
+    //     window.location.href = "https://nvqenn.com/";
+    //     //text.setAttribute("value", "https://softmind.tech");
+    //     currentTab = 'web';
+    // });
+    // emailButton.addEventListener('click', function (evt) {
+    //     window.location.href = "https://www.google.com/gmail/";
+    //     //text.setAttribute("value", "hello@softmind.tech");
+    //     currentTab = 'email';
+    // });
+    // locationButton.addEventListener('click', function (evt) {
+    //     window.location.href = "https://goo.gl/maps/PsyXqNNvgrY8YyF47";
+    //     console.log("loc");
+    //     //text.setAttribute("value", "Vancouver, Canada | Hong Kong");
+    //     currentTab = 'location';
+    // });
 
     // text.addEventListener('click', function (evt) {
     //     if (currentTab === 'web') {
@@ -53,51 +53,51 @@ const showInfo = () => {
     const portfolioRightButton = document.querySelector("#portfolio-right-button");
     const paintandquestPreviewButton = document.querySelector("#paintandquest-preview-button");
 
-    let y = 0;
-    let currentItem = 0;
+    // let y = 0;
+    // let currentItem = 0;
 
-    portfolio.setAttribute("visible", true);
+    // portfolio.setAttribute("visible", true);
 
-    const showPortfolioItem = (item) => {
-      for (let i = 0; i <= 7; i++) {
-        document.querySelector("#portfolio-item" + i).setAttribute("visible", i === item);
-      }
-    }
+    // const showPortfolioItem = (item) => {
+    //   for (let i = 0; i <= 7; i++) {
+    //     document.querySelector("#portfolio-item" + i).setAttribute("visible", i === item);
+    //   }
+    // }
 
-    const id = setInterval(() => {
-      y += 0.008;
-      if (y >= 0.6) {
-        clearInterval(id);
-        portfolioLeftButton.setAttribute("visible", true);
-        portfolioRightButton.setAttribute("visible", true);
-        portfolioLeftButton.addEventListener('click', () => {
-          currentItem = (currentItem + 1) % 8;
-          showPortfolioItem(currentItem);
-        });
-        portfolioRightButton.addEventListener('click', () => {
-          currentItem = (currentItem - 1 + 8) % 8;
-          showPortfolioItem(currentItem);
-        });
+    // const id = setInterval(() => {
+    //   y += 0.008;
+    //   if (y >= 0.6) {
+    //     clearInterval(id);
+    //     portfolioLeftButton.setAttribute("visible", true);
+    //     portfolioRightButton.setAttribute("visible", true);
+    //     portfolioLeftButton.addEventListener('click', () => {
+    //       currentItem = (currentItem + 1) % 8;
+    //       showPortfolioItem(currentItem);
+    //     });
+    //     portfolioRightButton.addEventListener('click', () => {
+    //       currentItem = (currentItem - 1 + 8) % 8;
+    //       showPortfolioItem(currentItem);
+    //     });
 
-        paintandquestPreviewButton.addEventListener('click', () => {
-          paintandquestPreviewButton.setAttribute("visible", false);
-          const testVideo = document.createElement( "video" );
-          const canplayWebm = testVideo.canPlayType( 'video/webm; codecs="vp8, vorbis"' );
-          if (canplayWebm == "") {
-            document.querySelector("#paintandquest-video-link").setAttribute("src", "#paintandquest-video-mp4");
-            document.querySelector("#paintandquest-video-mp4").play();
-          } else {
-            document.querySelector("#paintandquest-video-link").setAttribute("src", "#paintandquest-video-webm");
-            document.querySelector("#paintandquest-video-webm").play();
-          }
-        });
+    //     paintandquestPreviewButton.addEventListener('click', () => {
+    //       paintandquestPreviewButton.setAttribute("visible", false);
+    //       const testVideo = document.createElement( "video" );
+    //       const canplayWebm = testVideo.canPlayType( 'video/webm; codecs="vp8, vorbis"' );
+    //       if (canplayWebm == "") {
+    //         document.querySelector("#paintandquest-video-link").setAttribute("src", "#paintandquest-video-mp4");
+    //         document.querySelector("#paintandquest-video-mp4").play();
+    //       } else {
+    //         document.querySelector("#paintandquest-video-link").setAttribute("src", "#paintandquest-video-webm");
+    //         document.querySelector("#paintandquest-video-webm").play();
+    //       }
+    //     });
 
-        setTimeout(() => {
-          done();
-        }, 250);
-      }
-      //portfolio.setAttribute("position", "0 " + y + " -0.01");
-    }, 10);
+    //     setTimeout(() => {
+    //       done();
+    //     }, 250);
+    //   }
+    //   //portfolio.setAttribute("position", "0 " + y + " -0.01");
+    // }, 10);
   }
 
   const showAvatar = (onDone) => {
